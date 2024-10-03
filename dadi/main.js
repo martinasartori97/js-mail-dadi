46228dbdb3ae89070790f60d4d2015bd008716ba
martinasartori97 <martinasartori9@gmail.com>
2024-10-03 16:10:04 +0200

    edit js

--- dadi/main.js
+++ dadi/main.js
@@ -1,2 +1,10 @@
 // prova collegamento a javascript
 console.log("hello world");
+
+// gioco dei dadi
+// genero un numero random da 1 a 6
+const giocatore = Math.floor(Math.random() * 6)+1;
+const computer = Math.floor(Math.random() * 6)+1;
+
+
+
