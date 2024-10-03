a4ff56c271e69074fe8e1f40c34bda8cb69bbdf7
martinasartori97 <martinasartori9@gmail.com>
2024-10-03 15:23:25 +0200

    edit js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+// prova collegamento a javascript
 console.log("hello world");
 
+
+//chiedo all'utente di inserire la sua email
 let email = prompt("inserisci la tua email");
 console.log("email");
+
+// lista di email autorizzate
+ let authorizedEmail = ["esempio1@email.com","esempio2@gmail.com","esempio3@gmail.com"];
+ console.log(authorizedEmail);
+ 
+
 
 
