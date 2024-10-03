717d92dbbcffc187042406f897d6615bc99f5e0c
martinasartori97 <martinasartori9@gmail.com>
2024-10-03 15:52:40 +0200

    edit js

--- main.js
+++ main.js
@@ -7,19 +7,18 @@ let email = prompt("inserisci la tua email");
 console.log("email");
 
 // lista di email autorizzate
- let authorizedEmail = ["esempio1@email.com","esempio2@gmail.com","esempio3@gmail.com"];
- console.log(authorizedEmail);
+let authorizedEmail = ["esempio1@email.com", "esempio2@gmail.com", "esempio3@gmail.com"];
+console.log(authorizedEmail);
 
 // controllo se l'email è nella lista di quelle autorizzate
-if (authorizedEmail.includes(email)){
+if (authorizedEmail.includes(email)) {
     console.log("accesso consentito");
-    else{
+    else {
         console.log("accesso negato");
-        
+
     }
-    
 }
- 
+
 
 
 
